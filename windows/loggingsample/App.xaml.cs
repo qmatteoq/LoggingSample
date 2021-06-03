57b0abeeb60529d13fb4fa03d5f2a43c9a9c458d
Matteo Pagani <matteo.pagani@microsoft.com>
2021-06-03 19:21:20 +0200

    first commit

---
+++ windows/loggingsample/App.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.ReactNative;
+using NLog;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace loggingsample
+{
+    sealed partial class App : ReactApplication
+    {
+        public App()
+        {
+#if BUNDLE
+            JavaScriptBundleFile = "index.windows";
+            InstanceSettings.UseWebDebugger = false;
+            InstanceSettings.UseFastRefresh = false;
+#else
+            JavaScriptBundleFile = "index";
+            InstanceSettings.UseWebDebugger = true;
+            InstanceSettings.UseFastRefresh = true;
+#endif
+
+#if DEBUG
+            InstanceSettings.UseDeveloperSupport = true;
+#else
+            InstanceSettings.UseDeveloperSupport = false;
+#endif
+
+            Microsoft.ReactNative.Managed.AutolinkedNativeModules.RegisterAutolinkedNativeModulePackages(PackageProviders); // Includes any autolinked modules
+
+            PackageProviders.Add(new Microsoft.ReactNative.Managed.ReactPackageProvider());
+            PackageProviders.Add(new ReactPackageProvider());
+
+            UnhandledException += App_UnhandledException;
+
+            InitializeComponent();
+        }
+
+        private void App_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+            logger.Error(e);
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected async override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            base.OnLaunched(e);
+            await LoggingHelper.InitializeNLogAsync("App");
+            var frame = (Frame)Window.Current.Content;
+            frame.Navigate(typeof(MainPage), e.Arguments);
+        }
+
+        /// <summary>
+        /// Invoked when the application is activated by some means other than normal launching.
+        /// </summary>
+        protected override void OnActivated(IActivatedEventArgs e)
+        {
+            var preActivationContent = Window.Current.Content;
+            base.OnActivated(e);
+            if (preActivationContent == null && Window.Current != null)
+            {
+                // Display the initial content
+                var frame = (Frame)Window.Current.Content;
+                frame.Navigate(typeof(MainPage), null);
+            }
+        }
+    }
+}
---
+++ windows/loggingsample/LoggingHelper.cs
@@ -0,0 +1,49 @@
+﻿using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace loggingsample
+{
+    public static class LoggingHelper
+    {
+        private static async Task<StorageFolder> GetLogsFolder(string logFolder)
+        {
+            var storageFolder = ApplicationData.Current.LocalCacheFolder;
+            var logsFolder = await storageFolder.CreateFolderAsync("Logs", CreationCollisionOption.OpenIfExists);
+            var netLogsFolder = await logsFolder.CreateFolderAsync(logFolder, CreationCollisionOption.OpenIfExists);
+            return netLogsFolder;
+        }
+
+        public static async Task InitializeNLogAsync(string logName, string logFolder = "Uwp")
+        {
+            var logsFolder = await GetLogsFolder(logFolder);
+            var logsFilePath = Path.Combine(logsFolder.Path, $"{logName}.log");
+            var logsFilePathRolling = Path.Combine(logsFolder.Path, $"{logName}.{{#}}.log");
+
+            var config = new NLog.Config.LoggingConfiguration();
+
+            var logfile = new NLog.Targets.FileTarget("logfile")
+            {
+                FileName = logsFilePath,
+                ArchiveEvery = FileArchivePeriod.Day,
+                ArchiveNumbering = ArchiveNumberingMode.Date,
+                ArchiveDateFormat = "yyyyMMdd",
+                ArchiveFileKind = FilePathKind.Absolute,
+                ArchiveFileName = logsFilePathRolling,
+                MaxArchiveDays = 7,
+                Layout = "${longdate}|${level:uppercase=true}|${logger}|${message} ${exception:format=message}"
+            };
+
+
+            config.AddRuleForAllLevels(logfile);
+
+            NLog.LogManager.Configuration = config;
+        }
+    }
+
+}
